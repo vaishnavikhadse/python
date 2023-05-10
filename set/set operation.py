34cca0f372772c51f746f25faf4a26e058daf5c5
Khadse123 <akshaykhadse1110@gmail.com>
2023-05-10 16:41:38 +0530

    python program
    programs using set operations

---
+++ set/set operation.py
@@ -0,0 +1,10 @@
+s1={1,2,3,4}
+s2={4,5,6,7}
+print(s1.union(s2))
+print(s1.intersection(s2))
+print(s1.difference(s2))
+print(s1.symmetric_difference(s2))
+print(s1|s2)
+print(s1&s2)
+print(s1-s2)
+print(s1^s2)
